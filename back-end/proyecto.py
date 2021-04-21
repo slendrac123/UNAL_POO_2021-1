5afc60443240e3faceb46761e752d22dbcdb45ef
Juan J <juanjgarcialeon@gmail.com>
2021-04-20 19:30:18 -0500

    Creacion de modulo 2 sin guardado de imagen Arreglos menores en ingresos de la informacion

--- back-end/proyecto.py
+++ back-end/proyecto.py
@@ -1,6 +1,8 @@
 import sqlite3
 from sqlite3 import Error
 
+## TODO comments
+
 def sqlConnection():
     try:
         con = sqlite3.connect('./Vacunacion.db')
@@ -11,6 +13,7 @@ def sqlConnection():
 def crearTablas():
     con = sqlConnection()
     cursorObj = con.cursor()
+    # cursorObj.execute('''DROP TABLE pacientes''')
     cursorObj.execute('''
                 CREATE TABLE if not exists "pacientes" (
                     "noId"	NUMERIC(12),
@@ -27,9 +30,9 @@ def crearTablas():
                     PRIMARY KEY("noId")
                 );
                 ''')
-    # cursorObj.execute('''INSERT INTO "pacientes" VALUES (1001345205, 'juan jose', 'garcia leon', 'calle 17A #7b 57', 3108567898, 'jugarciale@unal.edu.co', 'bogota', '23112001', '05102014', 'N', '');''')
+    # cursorObj.execute('''DROP TABLE lote_vacunas''')
     cursorObj.execute('''
-                CREATE TABLE if not exists "lote_vacuna" (
+                CREATE TABLE if not exists "lote_vacunas" (
                     "noLote"	NUMERIC(12),
                     "fabricante"	CHAR(12),
                     "tipoVacuna"	CHAR(21),
@@ -40,10 +43,11 @@ def crearTablas():
                     "efectividad"	NUMERIC(2,1),
                     "tiempoProteccion"	NUMERIC(3),
                     "fechaVencimiento"	CHAR(10),
-                    "imagen"	IMAGE,
+                    "imagen"	LARGEBLOB,
                     PRIMARY KEY("noLote")
                 );
                 ''')
+    # cursorObj.execute('''DROP TABLE plan_vacunacion''')
     cursorObj.execute('''
                 CREATE TABLE if not exists "plan_vacunacion" (
                     "idPlan"	NUMERIC(2),
@@ -54,6 +58,7 @@ def crearTablas():
                     PRIMARY KEY("idPlan")
                 );
                 ''')
+    # cursorObj.execute('''DROP TABLE programacion_vacunas''')
     cursorObj.execute('''
                 CREATE TABLE if not exists "programacion_vacunas" (
                     "ciudadVacunacion"	CHAR(20),
@@ -79,34 +84,34 @@ def crearTablas():
 
 def menuModuloUno():
     while True:
-        opcion = int(input('Ingrese el numero de la opcion que desea realizar:\n1. Crear nuevo afiliado\n2. Consultar afiliado\n3. Desafiliar usuario\n4. Salir\n'))
-        if (opcion == 1): crearUsuario()
-        if (opcion == 2): consultarUsuario()
-        if (opcion == 3): desafiliarUsuario()
-        if (opcion == 4): break
+        opcion = input('Ingrese el numero de la opcion que desea realizar:\n1. Crear nuevo afiliado\n2. Consultar afiliado\n3. Desafiliar usuario\n4. Salir\n')
+        if opcion != '': 
+            opcion = int(opcion)
+            if (opcion == 1): crearUsuario()
+            if (opcion == 2): consultarUsuario()
+            if (opcion == 3): desafiliarUsuario()
+            if (opcion == 4): break
+        else: continue
 
 def crearUsuario():
-    con = sqlConnection()
+    con = sqlConnection() 
     cursorObj = con.cursor()
     print('Ingrese a continuacion los datos de la persona que desea registrar:')
     documentoID = int(input('Documento de Identidad:\n'))
-    # cursorObj.execute('SELECT count(*) FROM pacientes WHERE noId = {}'.format(documentoID))
-    # verificacion = int(cursorObj.fetchall()[0][0])
-    # if verificacion == 0:
     cursorObj.execute('SELECT * FROM pacientes WHERE noId = {}'.format(documentoID))
     resultado = cursorObj.fetchall()
     if len(resultado) == 0:
-        nombre = input('Nombre:\n')
-        apellido = input('Apellido:\n')
-        direccion = input('Direccion:\n')
+        nombre = input('Nombre:\n').title()
+        apellido = input('Apellido:\n').title()
+        direccion = input('Direccion:\n').title()
         telefono = int(input('Telefono:\n'))
         correo = input('Correo:\n')
-        ciudad = input('Ciudad:\n')
+        ciudad = input('Ciudad:\n').title()
         fechaNacimiento = input('Fecha de nacimiento (DDMMAAAA):\n')
         fechaAfiliacion = input('Fecha de afiliacion (DDMMAAAA):\n')
-        vacunado = input('¿Ha sido vacunado?:\n')
+        vacunado = input('¿Ha sido vacunado? (S/N):\n').title()
         fechaDesafiliacion = input('Fecha de desafiliacion en caso de haberla (DDMMAAAA):\n')
-        cursorObj.execute('INSERT INTO pacientes VALUES ("{a}","{b}","{c}","{d}","{e}","{f}","{g}","{h}","{i}","{j}","{k}")'.format(a=documentoID, b=nombre, c=apellido, d=direccion, e=telefono, f=correo, g=ciudad, h =fechaNacimiento, i=fechaAfiliacion, j=vacunado, k=fechaDesafiliacion))
+        cursorObj.execute('INSERT INTO pacientes VALUES ({a},"{b}","{c}","{d}",{e},"{f}","{g}","{h}","{i}","{j}","{k}")'.format(a=documentoID, b=nombre, c=apellido, d=direccion, e=telefono, f=correo, g=ciudad, h =fechaNacimiento, i=fechaAfiliacion, j=vacunado, k=fechaDesafiliacion))
         con.commit()
     else:
         print('Este usuario ya existe\n')
@@ -117,9 +122,10 @@ def consultarUsuario():
     con = sqlConnection()
     cursorObj = con.cursor()
     print('Ingrese a continuacion los datos de la persona que desea consultar:')
-    documentoID = int(input('Documento de Identidad:\n'))
+    documentoID = int(input('Documento de Identidad: '))
     cursorObj.execute('SELECT * FROM pacientes WHERE noId = {}'.format(documentoID))
     resultado = cursorObj.fetchall()
+    print('\n')
     if len(resultado) != 0:
         for datos in resultado[0]:
             if datos != '': print(datos)
@@ -137,11 +143,70 @@ def desafiliarUsuario():
     resultado = cursorObj.fetchall()
     if len(resultado) != 0:
         fechaDesafiliacion = input('Fecha de desafiliacion (DDMMAAAA):\n')
-        cursorObj.execute('UPDATE pacientes SET fechaDesafiliacion = {} WHERE noID = {}'.format(fechaDesafiliacion, documentoID))
+        cursorObj.execute('UPDATE pacientes SET fechaDesafiliacion = "{}" WHERE noID = {}'.format(fechaDesafiliacion, documentoID))
         con.commit()
     else: print('El paciente no se encuentra en los registros.')
 
     con.close()
 
+def menuModuloDos():
+    while True:
+        opcion = input('Ingrese el numero de la opcion que desea realizar:\n1. Crear nuevo lote de vacunas\n2. Consultar lote de vacunas\n3. Salir\n')
+        if opcion != '': 
+            opcion = int(opcion)
+            if (opcion == 1): crearLote()
+            if (opcion == 2): consultarLote()
+            if (opcion == 3): break
+        else: continue
+
+def crearLote():
+    con = sqlConnection() 
+    cursorObj = con.cursor()
+    print('Ingrese a continuacion los datos del lote que desea registrar:')
+    numeroLote = int(input('Numero del lote:\n'))
+    cursorObj.execute('SELECT * FROM lote_vacunas WHERE noLote = {}'.format(numeroLote))
+    resultado = cursorObj.fetchall()
+    if len(resultado) == 0:
+        fabricante = input('Fabricante:\n').title()
+        tipoVacuna = input('Tipo de vacuna:\n').title()
+        cantidadRecibida = int(input('Cantidad de vacunas recibidas:\n'))
+        cantidadUsada = int(input('Cantidad de vacunas usadas:\n'))
+        dosisNecesaria = int(input('Dosis necesarias:\n'))
+        temperatura = float(input('Temperatura de almacenamiento:\n'))
+        efectividad = float(input('Efectividad de la vacuna:\n'))
+        tiempoProteccion = int(input('Tiempo de proteccion (meses):\n'))
+        fechaVencimiento = input('Fecha de vencimiento:\n')
+        rutaImagen = input('Ruta completa a la imagen:\n')
+        with open(rutaImagen, "rb") as File:
+            imagenBinaria = File.read()
+        # print(imagenBinaria)
+        # print(type(imagenBinaria))
+        # Direccion de prueba: /home/alpha23/Pictures/Screenshot_20210316_011734.png
+
+        # # TODO Encontrar forma de almacenar la imagen
+        cursorObj.execute('INSERT INTO lote_vacunas VALUES ({a},"{b}","{c}",{d},{e},{f},{g},{h},{i},"{j}","")'.format(a=numeroLote, b=fabricante, c=tipoVacuna, d=cantidadRecibida, e=cantidadUsada, f=dosisNecesaria, g=temperatura, h =efectividad, i=tiempoProteccion, j=fechaVencimiento, k=imagenBinaria))
+        con.commit()
+    else:
+        print('Este lote de vacunas ya existe\n')
+    
+    con.close()
+
+def consultarLote():
+    con = sqlConnection()
+    cursorObj = con.cursor()
+    print('Ingrese a continuacion los datos del lote que desea consultar:')
+    noLote = int(input('Numero de lote: '))
+    cursorObj.execute('SELECT * FROM lote_vacunas WHERE noLote = {}'.format(noLote))
+    resultado = cursorObj.fetchall()
+    print('\n')
+    if len(resultado) != 0:
+        for datos in resultado[0]:
+            if datos != '': print(datos)
+        print('\n')
+    else: print('El lote no se encuentra registrado.\n')
+
+    con.close()
+
 crearTablas()
 menuModuloUno()
+menuModuloDos()
