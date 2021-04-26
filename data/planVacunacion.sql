e99175b4c3d8f2303fef23229fa6548e02022540
Juan J <juanjgarcialeon@gmail.com>
2021-04-26 02:09:25 -0500

    Datos plan vacunacion

---
+++ data/planVacunacion.sql
@@ -0,0 +1,4 @@
+INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('1', '60', '150', '2021-05-01', '2100-05-01');
+INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('2', '40', '59', '2021-07-01', '2100-07-01');
+INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('3', '20', '39', '2021-09-01', '2100-09-01');
+INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('4', '10', '19', '2021-01-01', '2021-11-01');
